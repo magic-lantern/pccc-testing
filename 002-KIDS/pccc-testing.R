7ed4c299783d1d57d8198d0271aa99b088b1719e
Seth Russell <magic-lantern@users.noreply.github.com>
2017-09-19 10:39:28 -0600

    Adding more validation and saving of variables for comparison purposes

--- 002-KIDS/pccc-testing.R
+++ 002-KIDS/pccc-testing.R
@@ -5,8 +5,8 @@
 # 3. "C:\Programs Files\R\R-3.3.3\bin\R.exe" CMD BATCH --vanilla pccc-testing.R
 # 4. Review the pccc-testing.Rout file.
 
-
 # pccc test using the KID09 data
+
 date()
 
 library(readr, lib.loc = "C:/R-site-library/")
@@ -43,7 +43,41 @@ system.time({
 
 kid_ccc
 
-dplyr::summarize_at(kid_ccc, vars(-recnum), sum) %>% print.data.frame
+sum_results <- dplyr::summarize_at(kid_ccc, vars(-recnum), sum) %>% print.data.frame
 dplyr::summarize_at(kid_ccc, vars(-recnum), mean) %>% print.data.frame
 
+# this is the results from the SAS run - output from this R script should match exactly
+# will only match when run against the full dataset
+sas_sums = tibble(
+  Neuromuscular = 112619L,
+  CVD = 116094L,
+  Respiratory = 47942L,
+  Renal = 60456L,
+  GI = 99462L,
+  Hemato_immu = 79081L,
+  Metabolic = 55830L,
+  Congeni_genetic = 81622L,
+  Malignancy = 80360L,
+  Neonatal = 58608L,
+  Tech_dep = 129164L,
+  Transplant = 18342L
+)
+
+# don't have validated number for ccc_flag column
+sum_results2 <- subset( sum_results, select = -ccc_flag)
+
+identicalVars <-
+  sapply(colnames(sum_results2), function(x){
+    identical(sum_results2[[x]], sas_sums[[x]])
+  })
+
+mismatch <- names(identicalVars[!identicalVars])
+print("R version: ")
+print(sum_results2[mismatch])
+print("SAS version: ")
+print(sas_sums[mismatch])
+
 print(sessionInfo(), locale = FALSE)
+
+# run this line to clear the environment - it's pretty large so might want to save
+# rm(list=ls())
