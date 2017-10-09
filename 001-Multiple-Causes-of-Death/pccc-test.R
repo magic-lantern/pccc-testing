39bbf30bec7a48e8b59a19dfb01e9ba1212a5a74
Seth Russell <magic-lantern@users.noreply.github.com>
2017-10-09 13:50:15 -0600

    Saving new version of file to work with new version of data file provided by Jamie Feinstein

--- 001-Multiple-Causes-of-Death/pccc-test.R
+++ 001-Multiple-Causes-of-Death/pccc-test.R
@@ -1,19 +1,19 @@
 # Packages
 library(pccc)
-library(readr)
 library(dplyr)   
 library(tictoc)  # timing tools
-library(magrittr)
 library(qwraps2)
-library(survey)
 
 # Data import
+# data in file:
+#   cause (underlying cause of death, ICD-9)
+#   recax1-recax20 (axis conditions, ICD-9)
+#   uc10 (underlying cause of death, ICD-10)
+#   rax10_1-rax10_20 (axis conditions, ICD-10)
 tic()
-dat <-
-  readr::read_csv("codes.dat", trim_ws = TRUE,
-                  col_types = paste(rep("c", 84), collapse = "")) %>%
-  dplyr::filter(is.na(Comp_flag)) %>%
-  dplyr::mutate(id = seq_along(Comp_flag))
+dat <- readRDS("C:/HCUPData/MCOD/mcod_icd_9_10.rds") %>%
+       dplyr::mutate(id = seq_along(cause))
+dat <- dat[colnames(dat)[c(ncol(dat),1:ncol(dat) - 1)]]
 toc()
 
 # Run ccc on the ICD-9 codes
@@ -21,21 +21,30 @@ tic()
 icd09_results <-
   ccc(dat,
       id = id,
-      dx_cols = dplyr::vars(dplyr::starts_with("ICD9")),
-      pc_cols = dplyr::vars(dplyr::starts_with("ICD9")),
+      dx_cols = dplyr::vars(matches("recax|cause")),
       icdv = 9)
 toc()
+
+icd09_pct <- dplyr::summarize_at(icd09_results, vars(-recnum), sum) %>% print.data.frame
+dplyr::summarize_at(icd09_results, vars(-id), mean) %>% print.data.frame
 
 # Run ccc on the ICD-10 codes
 tic()
 icd10_results <-
   ccc(dat,
       id = id,
-      dx_cols = dplyr::vars(dplyr::starts_with("ICD10")),
-      pc_cols = dplyr::vars(dplyr::starts_with("ICD10")),
+      dx_cols = dplyr::vars(matches('rax10|uc10')),
       icdv = 10)
 toc()
 
-save(dat, icd09_results, icd10_results, file = "icd09_v_icd10.RData")
+icd10_pct <- dplyr::summarize_at(icd10_results, vars(-recnum), sum) %>% print.data.frame
+dplyr::summarize_at(icd10_results, vars(-id), mean) %>% print.data.frame
+
+
+saveRDS(icd09_results, file = "mcod_icd09_results.rds")
+saveRDS(icd10_results, file = "mcod_icd10_results.rds")
 
 print(sessionInfo(), locale = FALSE)
+
+
+save(dat, icd09_results, icd10_results, file = "icd09_v_icd10.RData")
